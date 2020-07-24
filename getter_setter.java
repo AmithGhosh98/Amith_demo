2f8b636e69df459b10bffda6881b05218ae15956
AmithGhosh98 <68325807+AmithGhosh98@users.noreply.github.com>
2020-07-24 19:52:27 +0530

    Add files via upload

---
+++ getter_setter.java
@@ -0,0 +1,24 @@
+
+public class getter_setter {
+	String name;
+	int roll_no;
+	int marks;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getRoll_no() {
+		return roll_no;
+	}
+	public void setRoll_no(int roll_no) {
+		this.roll_no = roll_no;
+	}
+	public int getMarks() {
+		return marks;
+	}
+	public void setMarks(int marks) {
+		this.marks = marks;
+	}
+}
